5f366d54a94a9fed1ceb0ec271251855006f3282
RohitSolan08 <rohitsolan08@gmail.com>
2021-09-21 13:53:24 +0530

    CodeForces Solution

---
+++ HexadecimalTheorem.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class HexadecimalTheorem {
+public static void main (String args[])
+{
+Scanner in=new Scanner(System.in);
+int n;
+n=in.nextInt();
+System.out.println("0 0 "+n);
+}
+}
