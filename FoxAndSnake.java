c9fbec521194d7d97cef7f2f9dee9b3dc989ab91
RohitSolan08 <rohitsolan08@gmail.com>
2021-10-03 11:54:16 +0530

    CodeForces Solution

---
+++ FoxAndSnake.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class  FoxAndSnake {
+
+    public static void main(String[] args){
+        
+       
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+ 
+        int mark=0;
+ 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i%2==0)
+                    System.out.print("#");
+                else if(i%2==1 && mark==0){
+                    if(j<m-1)
+                        System.out.print(".");
+                    else{
+                        mark =1;
+                        System.out.print("#");
+                    }
+                }
+                else if(i%2==1 && mark==1){
+                    if(j==0)
+                        System.out.print("#");
+                    else
+                        System.out.print(".");
+                    if(j==m-1)
+                        mark=0;
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
