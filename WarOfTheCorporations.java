4bf412eb6d295646abf09fe2a02d3470f35e8c9c
RohitSolan08 <rohitsolan08@gmail.com>
2021-10-22 01:46:48 +0530

    Codeforces

---
+++ WarOfTheCorporations.java
@@ -0,0 +1,30 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class  WarOfTheCorporations
+{
+    public static void main(String[] args){
+        
+    int i,j,k;
+        String g,a;
+        
+        Scanner in=new Scanner(System.in);
+        
+        g=in.nextLine();
+        a=in.nextLine();
+        
+        int index=g.indexOf(a);
+        int count=0;
+        int len=g.length();
+        
+        while(index<len && index>-1)
+        {
+            index=g.indexOf(a,index+a.length());
+            count++;
+        }
+        
+        System.out.print(count);
+       
+    }
+}
