5e1ac67e323800a8aff989d032747538a1098140
RohitSolan08 <rohitsolan08@gmail.com>
2021-10-31 23:29:23 +0530

    COsde

---
+++ TavasAndSaDDas.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.io.*;
+
+public class TavasAndSaDDas {
+	static long[] arr;
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		long lNum = Long.parseLong(br.readLine());
+		arr = new long[1023];
+		
+		dfs(0, 0);
+
+		HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+		for (int i = 1; i < arr.length; i++)
+			map.put(arr[i], i);
+
+		System.out.println(map.get(lNum));
+	}
+
+	static void dfs(long n, int k) {
+		if (n > 1000000000)
+			return;
+		arr[k] = n;
+		dfs(n * 10 + 4, k * 2 + 1);
+		dfs(n * 10 + 7, k * 2 + 2);
+	}
+}
