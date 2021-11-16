1c1680ac10e4553aaee5a7d77ef27a3dfb3b3ce1
RohitSolan08 <rohitsolan08@gmail.com>
2021-11-16 21:47:45 +0530

    Code

---
+++ Elephant.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class   Elephant{
+
+    public static void main(String[] args){
+        
+         Scanner sc = new Scanner(System.in);
+          
+          int n = sc.nextInt();
+          
+          System.out.println((n+4)/5);
+         }
+}
+
