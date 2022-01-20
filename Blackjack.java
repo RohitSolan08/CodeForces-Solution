2bd778ebffaf9827322b197077f8d68ec3371d5b
RohitSolan08 <rohitsolan08@gmail.com>
2022-01-20 14:15:07 +0530

    Code

---
+++ Blackjack.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class  Blackjack {
+
+    public static void main(String[] args) 
+    {
+        
+     Scanner sc=new Scanner(System.in);
+     int n = sc.nextInt();
+     
+     if(n<=10 || n >21)
+     {
+         System.out.println(0);
+     }
+     else if(n ==20)
+     {
+         System.out.println(15);
+     }
+     else
+     {
+         System.out.println(4);
+     }
+     
+    
+     }
+
+}
