80bc75204516f59af9ac34b3c7cdfa99cc48cc7a
RohitSolan08 <rohitsolan08@gmail.com>
2022-01-24 11:46:33 +0530

    Code

---
+++ HexadecimalTheorems.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class  HexadecimalTheorems {
+
+    public static void main(String[] args) 
+    {
+        
+     Scanner sc=new Scanner(System.in);
+     int n = sc.nextInt();
+     
+     System.out.println(0+ " "+ 0+ " "+ n);
+     }
+
+}
